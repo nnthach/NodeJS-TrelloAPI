bcaf264dd301ac8d2a7ff22b32123501e53a9f6c
nnthach <nguyenngocthach2301@gmail.com>
2025-03-06 13:32:28 +0700

    Connect database

--- src/server.js
+++ src/server.js
@@ -1,12 +1,30 @@
 import express from "express";
+import { CONNECT_DB, GET_DB } from "~/config/mongodb";
 
-const app = express();
-const port = 2301;
+const START_SERVER = () => {
+  const app = express();
+  const port = 2301;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+  app.get("/", async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray());
+    res.send("Hello World!");
+  });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  app.listen(port, () => {
+    console.log(`3. Hello! Let's start your app on port ${port}`);
+  });
+};
+
+(async () => {
+  try {
+    console.log("1. Connecting to MongoDB CLoud Atlas");
+    await CONNECT_DB();
+    console.log("2. Connect success to MongoDB Cloud Atlas");
+
+    START_SERVER();
+  } catch (e) {
+    console.log(e);
+    process.exit(0);
+  }
+})();
+
